56c234ac618e42f584c9b9d85c5d0455d2794279
ARC Researc <duybmthoang@gmail.com>
2026-03-14 15:09:55 +0700

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Shelby Crypto Dashboard</h1>
+      <p>Real-time cryptocurrency analytics platform.</p>
+    </div>
+  );
+}
+
+export default App
